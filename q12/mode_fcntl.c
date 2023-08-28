863bb73fe5c31046098bed117607629698e1a63e
kartikgawande <kartikgawandeonline@gmail.com>
2023-08-28 23:04:50 +0530

    C program to find access mode of a file

---
+++ q12/mode_fcntl.c
@@ -0,0 +1,35 @@
+#include<fcntl.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc, char* argv[]){
+	int fd = open("file.txt", O_RDWR);
+	int statusFlag = fcntl(fd, F_GETFL);
+	int accessMode = statusFlag & O_ACCMODE;
+	
+	printf("fd:%d\n", fd);
+	printf("statusFlag:%d\n", statusFlag);
+	printf("accessMode:%d\n", accessMode);
+	
+	/*switch(accessMode){
+		case O_RDONLY:
+			printf("Read only.");
+			break;
+		case O_WRONLY:
+			printf("Write only.");
+			break;
+		case O_RDWR:
+			printf("Read and write.");
+			break;
+		default:
+			printf("Other modes.");
+			break;
+	}*/
+	
+	if(accessMode==O_RDONLY)printf("Read only.\n");
+	else if(accessMode==O_WRONLY)printf("Write only.\n");
+	else if(accessMode==O_RDWR)printf("Read and write only.\n");
+	else printf("Other mode.\n");
+	close(fd);
+	return 0;
+}
