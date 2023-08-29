79c587f11f6a80d03c4bdb95c89cc3f1cc196bba
kartikgawande <kartikgawandeonline@gmail.com>
2023-08-29 15:36:32 +0530

    C program to identify file type

---
+++ q14/type.c
@@ -0,0 +1,18 @@
+#include<sys/stat.h>
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	char* file = argv[1];
+	struct stat finfo;
+	stat(file, &finfo);
+	
+	if(S_ISREG(finfo.st_mode)) printf("Regular.");
+	else if(S_ISDIR(finfo.st_mode)) printf("Directory.");
+	else if(S_ISCHR(finfo.st_mode)) printf("Char special.");
+	else if(S_ISBLK(finfo.st_mode)) printf("Block special.");
+	else if(S_ISFIFO(finfo.st_mode)) printf("FIFO.");
+	else if(S_ISSOCK(finfo.st_mode)) printf("Socket.");
+	else if(S_ISLNK(finfo.st_mode)) printf("Symlink.");
+	else printf("Unknown.");
+}
